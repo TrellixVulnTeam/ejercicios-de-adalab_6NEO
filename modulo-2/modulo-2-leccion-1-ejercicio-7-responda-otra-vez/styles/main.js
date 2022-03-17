11c1cd9bb4cee59829f319f9ad6cfddc434555fa
SandraRial <sandrarialperez@gmail.com>
2022-03-17 17:54:01 +0100

    add list elements

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-7-responda-otra-vez/styles/main.js
@@ -0,0 +1,7 @@
+"user strict";
+
+const listElement = document.querySelector("ul");
+
+const content = '<h1>Lista de elementos</h1><li>Texto 1</li><li>Texto 2</li><li>Texto 3</li>';
+
+listElement.innerHTML = content;
