e0d8ad3cdab06a0895e6ef5aa63fa626b4dbd262
SandraRial <sandrarialperez@gmail.com>
2022-03-17 17:11:10 +0100

    Adalabers list js

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-4-seleccionando-adalabers/styles/main.js
@@ -0,0 +1,8 @@
+"user strict";
+
+//Intento de ponerlo como variable y que cogiese así el segundo nombre
+let titleElement = document.querySelector(".title");
+
+titleElement.innerHTML = titleElement.innerHTML + " Lola";
+
+titleElement.innerHTML = titleElement.innerHTML + "Manola";
