5bf20735cbf2b41f12978f757c199f3b8d4db66d
SandraRial <sandrarialperez@gmail.com>
2022-03-18 20:06:37 +0100

    proving lenth

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-2.3.4-lenth-nombre/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const name = "Leticia Fernández Sánchez";
+
+console.log("El nombre de mi compañera es" + name.lenth + "y está compuesto por 23 caracteres");
+
+//No me suma los caracteres del nombre en la consola
