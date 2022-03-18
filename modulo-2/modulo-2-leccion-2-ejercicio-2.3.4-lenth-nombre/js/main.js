68004f5f8540e0629f7fdb83b632d47cca34738a
SandraRial <sandrarialperez@gmail.com>
2022-03-18 20:26:19 +0100

    add comment

--- modulo-2/modulo-2-leccion-2-ejercicio-2.3.4-lenth-nombre/js/main.js
+++ modulo-2/modulo-2-leccion-2-ejercicio-2.3.4-lenth-nombre/js/main.js
@@ -5,3 +5,5 @@ const name = "Leticia Fernández Sánchez";
 console.log("El nombre de mi compañera es" + name.lenth + "y está compuesto por 23 caracteres");
 
 //No me suma los caracteres del nombre en la consola
+
+//Ha que meter parseIn para que convierta un string a numeros
