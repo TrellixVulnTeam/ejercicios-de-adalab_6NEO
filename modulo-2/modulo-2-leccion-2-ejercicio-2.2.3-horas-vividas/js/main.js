5805688755bb0ab174c03637ecf6ebab8dc29c57
SandraRial <sandrarialperez@gmail.com>
2022-03-18 18:10:49 +0100

    prove console

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-2.2.3-horas-vividas/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const day = 24;
+
+const week = 7;
+
+const month = 30;
+
+const year = 12;
+
+const life = 60;
+
+console.log(day * week * month * year * life);
+ 
+//La operación está mal hecha, pero el lenguaje de js para multiplicar se llevaría a cabo así
+
+//En la consola aparece el resultado
