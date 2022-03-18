79dfb7e9207641d97f5dce9ec5bc0452ed519394
SandraRial <sandrarialperez@gmail.com>
2022-03-18 19:17:25 +0100

    interpolate lola

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-2.3.1-hola-adalaber/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "Lola";
+
+const titleElement = document.querySelector(".greet");
+
+titleElement.innerHTML = "Hola Lola, encanta de conocerte";
+
+//console.log(`Hola ${name}, encantada de conocerte.`);
+
+//En la consola aparece: Hola Lola, encantada de conocerte.
