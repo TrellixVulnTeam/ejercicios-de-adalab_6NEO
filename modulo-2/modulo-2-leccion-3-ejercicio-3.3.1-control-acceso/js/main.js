41e8595d6ca46e6f2a42b4abe8d74c0acd94913f
SandraRial <sandrarialperez@gmail.com>
2022-03-21 19:47:11 +0100

    add conditional exercise

---
+++ modulo-2/modulo-2-leccion-3-ejercicio-3.3.1-control-acceso/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "Sandra";
+
+if (name <= "Sandra") {
+    console.log("Bienvenida, Sandra!");
+} else {
+    console.log("Lo siento ha ocurrido un error.")
+}
+
+//Al poner <= en la consola aparece Bienvenida Sandra
