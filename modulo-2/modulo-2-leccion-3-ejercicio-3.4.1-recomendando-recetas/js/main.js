41666aaa783f418195aaee9871721946ecc31dd2
SandraRial <sandrarialperez@gmail.com>
2022-03-21 20:17:02 +0100

    add else if exercise

---
+++ modulo-2/modulo-2-leccion-3-ejercicio-3.4.1-recomendando-recetas/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let ingredient = "Pollo";
+
+if (ingredient === "Merluza") {
+    console.log("Puedes preparar merluza en salsa verde");
+} else if (ingredient === "Pollo") {
+    console.log("Prepara un pollo al curry");
+} else if (ingredient === "Espinacas") {
+    console.log("Espinacas rehogadas");
+}
