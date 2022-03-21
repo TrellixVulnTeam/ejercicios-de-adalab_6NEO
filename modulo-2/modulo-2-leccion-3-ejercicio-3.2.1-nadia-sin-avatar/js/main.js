6160e69088be99214eff5d4f7db7c3ce9360ad36
SandraRial <sandrarialperez@gmail.com>
2022-03-21 20:50:37 +0100

    update

--- modulo-2/modulo-2-leccion-3-ejercicio-3.2.1-nadia-sin-avatar/js/main.js
+++ modulo-2/modulo-2-leccion-3-ejercicio-3.2.1-nadia-sin-avatar/js/main.js
@@ -4,5 +4,12 @@
 const DEFAULT_AVATAR = "http://placehold.it/300x300";
 
 // avatar que eligió el usuario al registrarse
-let userAvatar = "http://www.fillmurray.com/300/300";
+const userAvatar = "http://www.fillmurray.com/300/300";
 
+document.querySelector(".user__avatar").src=`http://www.fillmurray.com/300/300`;
+
+DEFAULT_AVATAR === "http://placehold.it/300x300" || userAvatar === 0;
+
+userAvatar === "http://www.fillmurray.com/300/300" || DEFAULT_AVATAR === 0;
+
+// lines 11 y 13 creo que están mal
