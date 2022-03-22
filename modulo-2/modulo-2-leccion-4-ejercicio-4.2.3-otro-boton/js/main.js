f3d9eb31eb7a43f355cbf891236fa9458a2d9700
SandraRial <sandrarialperez@gmail.com>
2022-03-22 18:44:46 +0100

    update exercise

--- modulo-2/modulo-2-leccion-4-ejercicio-4.2.3-otro-boton/js/main.js
+++ modulo-2/modulo-2-leccion-4-ejercicio-4.2.3-otro-boton/js/main.js
@@ -1,6 +1,12 @@
 "use strict";
 
 const but = document.querySelector(".button");
-but.classList.toggle()
 
-//Me falta añadir clases de background cuando se pulsa click en el boton
+but.addEventListener("click", toggle);
+
+function toggle () {
+    console.log(but);
+    but.classList.toggle("button")
+}
+
+//Cuando le clickas en el boton que le quitan las clases, y cuando vuelves a clicar en el botón, se le vuelven a poner
