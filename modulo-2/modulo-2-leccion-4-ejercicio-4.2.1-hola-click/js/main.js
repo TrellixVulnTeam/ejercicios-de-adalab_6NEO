308206d385621389787a0b9a5e7c154626b33983
SandraRial <sandrarialperez@gmail.com>
2022-03-22 17:09:53 +0100

    add click event

---
+++ modulo-2/modulo-2-leccion-4-ejercicio-4.2.1-hola-click/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const button = document.querySelector(".click");
+
+button.addEventListener("click",()=>{ 
+console.log("Mi primer click,¡Ole yo!");
+});
+
+//Pinta en la consola: Mi primer click,¡Ole yo!
