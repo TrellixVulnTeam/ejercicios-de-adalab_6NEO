052c589d5443097d2a6fe7f1e7ecc205b0109f74
SandraRial <sandrarialperez@gmail.com>
2022-03-22 19:04:44 +0100

    mouseover

---
+++ modulo-2/modulo-2-leccion-4-ejercicio-4.2.4-dame-ipsum/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const parragraph = document.querySelector(".js-text");
+
+parragraph.addEventListener("mouseover", () => {
+    console.log(add = "lorem ipsum");
+});
+
+//No se como añadir otro lorem cuando el raton pase por encima del texto
