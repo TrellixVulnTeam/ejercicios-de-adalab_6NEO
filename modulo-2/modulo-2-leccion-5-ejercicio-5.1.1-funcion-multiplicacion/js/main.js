4fdb9b79b8e946224a58da6b13484470b497a369
SandraRial <sandrarialperez@gmail.com>
2022-03-23 18:18:19 +0100

    add multiply function exercise

---
+++ modulo-2/modulo-2-leccion-5-ejercicio-5.1.1-funcion-multiplicacion/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function multiply(a, b) {
+    const result = a * b;
+
+    return result
+}
+
+console.log(multiply(2, 5));
+console.log(multiply(6, 2));
+console.log(multiply(3, 3));
