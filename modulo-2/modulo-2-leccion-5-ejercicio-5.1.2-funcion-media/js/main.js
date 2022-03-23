2eee2942c0df4e5826bd21779b2f3c63a174f350
SandraRial <sandrarialperez@gmail.com>
2022-03-23 18:29:18 +0100

    add media

---
+++ modulo-2/modulo-2-leccion-5-ejercicio-5.1.2-funcion-media/js/main.js
@@ -0,0 +1,13 @@
+"use script";
+
+function media(a, b, c, d){
+    const result = ((a + b + c +d) / 4);
+
+    return result;
+}
+
+console.log(media(2, 3, 4, 5));
+console.log(media(5,3,6,8));
+console.log(media(4, 2, 6, 2));
+
+//Hace la suma de las cuatro letras y las divide entre 4, y te devuelve la media
