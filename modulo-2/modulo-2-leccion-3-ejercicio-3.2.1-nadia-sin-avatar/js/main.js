180b67b205977f7fb31c95cee8e82270c770a593
SandraRial <sandrarialperez@gmail.com>
2022-03-24 12:13:35 +0100

    update in class

--- modulo-2/modulo-2-leccion-3-ejercicio-3.2.1-nadia-sin-avatar/js/main.js
+++ modulo-2/modulo-2-leccion-3-ejercicio-3.2.1-nadia-sin-avatar/js/main.js
@@ -1,15 +1,31 @@
 "use strict";
 
+
+const avatarImage = document.querySelector('.js_image');
+
 // avatar por defecto
 const DEFAULT_AVATAR = "http://placehold.it/300x300";
 
 // avatar que eligió el usuario al registrarse
 const userAvatar = "http://www.fillmurray.com/300/300";
 
-document.querySelector(".user__avatar").src=`http://www.fillmurray.com/300/300`;
 
-DEFAULT_AVATAR === "http://placehold.it/300x300" || userAvatar === 0;
+// innerHTML nunca se usa con un input y img
 
-userAvatar === "http://www.fillmurray.com/300/300" || DEFAULT_AVATAR === 0;
+// console.log(avatarImage.src)-- muestra en la consola el src de imagen
 
-// lines 11 y 13 creo que están mal
+avatarImage.src = userAvatar;
+
+if ( userAvatar === '') {
+    // Poner el avatar por defecto
+avatarImage.src = DEFAULT_AVATAR;
+} else {
+// Poner el avatar de la usuaria
+avatarImage.src = userAvatar;
+}
+
+/* OPCION 2 - CON TERNARIO
+avatarImage.src = userAvatar === '' ? DEFAULT_AVATAR : userAvatar; */
+
+/* 
+avatarImage.setAtrivute('src', userAvatar || DEFAULT_AVATAR); */
