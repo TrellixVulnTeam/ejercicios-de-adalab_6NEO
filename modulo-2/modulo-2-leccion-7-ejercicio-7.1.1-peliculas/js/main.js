8cf44e0030be84d99bfefe650a603d7f3844d16d
SandraRial <sandrarialperez@gmail.com>
2022-03-25 19:48:31 +0100

    add arrays exercise

---
+++ modulo-2/modulo-2-leccion-7-ejercicio-7.1.1-peliculas/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Creamos un array con un listado de tres películas
+// const movies = ["Diario de Noa", "Origen", "Love Actually"];
+
+
+// // Añadimos una pelicula más al array
+// movies[3] = "Spiderman: No way home";
+
+
+// // Sustituímos una película que ya había, por una nueva
+// movies[2] = "Anastasia";
+// movies[0] = "Loki";
+
+
+// Encapsular todo el código que hemos creado en una función que no toma parámetros
+function workWithMovies () {
+    const movies = ["Diario de Noa", "Origen", "Love Actually"];
+    movies[3] = "Spiderman: No way home";
+    movies[2] = "Anastasia";
+    movies[0] = "Loki";
+
+}
+
+console.log(workWithMovies);
