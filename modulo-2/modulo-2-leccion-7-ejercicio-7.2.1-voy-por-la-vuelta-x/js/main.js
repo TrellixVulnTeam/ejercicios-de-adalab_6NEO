12d6e5a4315ecf27540bcf64c396224350392433
SandraRial <sandrarialperez@gmail.com>
2022-03-25 20:35:19 +0100

    add for exercise

---
+++ modulo-2/modulo-2-leccion-7-ejercicio-7.2.1-voy-por-la-vuelta-x/js/main.js
@@ -0,0 +1,7 @@
+'use stric';
+
+// Crear un bucle que muestr 10 veces, en la consola "voy por la vuelta x" siendo el número de vueltas desde la 1 hasta la 10 (no desde la 0 hasta la 9)
+
+for (let i = 1; i < 11; i++) {
+    console.log("Voy por la vuelta " + i);
+}
