6667d028838ed1acd7a346ec8bad51083513bf8f
SandraRial <sandrarialperez@gmail.com>
2022-03-27 17:04:25 +0200

    add array exercise

---
+++ modulo-2/modulo-2-leccion-7-ejercicio-7.3.1-story-of-adalabers/js/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const adalabers = [
+    {
+        name: "María",
+        age: 29,
+        job: "diseñadora",
+    },
+    {
+        name: "Lucía",
+        age: 31,
+        job: "ingeniera química",
+    },
+    {
+        name: "Susana",
+        age: 34,
+        job: "periodista",
+    },
+    {
+        name: "Rocío",
+        age: 25,
+        job: "actriz",
+    },
+    {
+        name: "Inmaculada",
+        age: 21,
+        job: "diseñadora",
+    },
+];
+
+//Crear una función que devuelve el número de adalabers en el listado
+function countAdalabers() {
+    console.log(adalabers.length);
+};
+
+//Crear una función que devuelve la media de edad del listado
+function averAge() {
+    
+}
+
+//Crear una fución que devuelve el nombre de la adalaber más joven
+function theYoungest () {
+    
+}
+
+// Crear una función que devuelve el número de adalabers que son diseñadoras
+function countDesigners() {
+    
+}
