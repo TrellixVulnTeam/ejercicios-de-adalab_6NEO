3800accb707a12bd3e1967005914abdbb2fe032c
SandraRial <sandrarialperez@gmail.com>
2022-04-01 16:32:59 +0200

    update exercise

--- modulo-2/modulo-2-leccion-11-ejercicio-11.1.1-numero-aleatorio/js/main.js
+++ modulo-2/modulo-2-leccion-11-ejercicio-11.1.1-numero-aleatorio/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+ const resultInteger = document.querySelector('.js-result');
 // function getEmoji() {
 //   fetch('https://api.rand.fun/games/rockpaperscissorslizardspock')
 //     .then((response) => response.json())
@@ -14,7 +14,7 @@ function integer() {
     fetch('https://api.rand.fun/number/integer')
     .then((response) => response.json())
     .then((data) => {
-        document.querySelector('.js_result').innerHTML = data.result;
+        resultInteger.innerHTML = data.result;
     });
 }
 document.querySelector('.js_integer').addEventListener('click', integer);
