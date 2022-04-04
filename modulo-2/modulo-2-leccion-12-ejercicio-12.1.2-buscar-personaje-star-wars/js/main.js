f771ecc14919a8d44a8e0277ee6edea23eb754a0
SandraRial <sandrarialperez@gmail.com>
2022-04-04 19:18:28 +0200

    add exercise

---
+++ modulo-2/modulo-2-leccion-12-ejercicio-12.1.2-buscar-personaje-star-wars/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const inputElement = document.querySelector('.js_input');
+const btn = document.querySelector('.js_button');
+
+function handleClickBtn() {
+  fetch;
+}
+
+btn.addEventListener('click', handleClickBtn);
