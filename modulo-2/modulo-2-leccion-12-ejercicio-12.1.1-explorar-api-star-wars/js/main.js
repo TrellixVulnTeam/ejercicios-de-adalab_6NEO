f6e739f29bfdd770643b205bcdb3afe7e3975e79
SandraRial <sandrarialperez@gmail.com>
2022-04-04 17:55:57 +0200

    add api exercise

---
+++ modulo-2/modulo-2-leccion-12-ejercicio-12.1.1-explorar-api-star-wars/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// 1- la URL base del API
+//https://swapi.py4e.com/api/
+
+// 2 - si necesita autenticación
+//Swapi is a completely open API. No authentication is required to query and get data.
+
+// 3 - método HTTP que deben usar las peticiones a este API
+//Making a request to /api/<resource>/schema will give you the details of that resource
+
+// 4 - URL para acceder a la info del personaje "Luke Skywalker"
+//https://swapi.py4e.com/api/people/1
+
+// 5 - URL para acceder a la info de la película "A New Hope"
+//https://swapi.py4e.com/api/films/1/
+
+// 6 - a qué otros recursos puedo acceder desde el API además de personajes y pelis
+// A sus naves estalelares, a sus vehiculos, a las especies y planetas
+
+// 7 - URL para acceder al listado de personajes, ¿está paginada?
+//si
+
+// 8 -cómo puedo buscar personajes mediante la URL
+// poniendole un numero despues de /people/
+
+// 9 - cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee
+// Añadiendo  /?format=wookiee/ a la url-- Ej:https://swapi.py4e.com/api/planets/1/?format=wookiee
