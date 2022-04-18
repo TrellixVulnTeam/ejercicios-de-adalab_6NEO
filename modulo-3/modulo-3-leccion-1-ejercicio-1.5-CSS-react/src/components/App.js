4c12e84f90d9996438fad4f662311e016d916ee6
SandraRial <sandrarialperez@gmail.com>
2022-04-18 19:08:19 +0200

    add css to react

---
+++ modulo-3/modulo-3-leccion-1-ejercicio-1.5-CSS-react/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="title">Esta es mi primera página con React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
