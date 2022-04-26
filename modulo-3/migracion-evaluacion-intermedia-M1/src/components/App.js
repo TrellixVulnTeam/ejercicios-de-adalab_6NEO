626401791085d3b1e090d24da918969bd90cedf0
SandraRial <sandrarialperez@gmail.com>
2022-04-26 16:52:27 +0200

    add template

---
+++ modulo-3/migracion-evaluacion-intermedia-M1/src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.scss';
+// import callToApi from '../services/api';
+// import { useEffect, useState } from 'react';
+// import ls from '../services/localStorage';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
