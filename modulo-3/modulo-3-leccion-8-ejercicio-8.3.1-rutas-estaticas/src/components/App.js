a72d8a512100889c69fd7636f88fb6b3be88c77d
SandraRial <sandrarialperez@gmail.com>
2022-04-28 18:17:54 +0200

    add routes exercise

--- modulo-3/modulo-3-leccion-8-ejercicio-8.3.1-rutas-estaticas/src/components/App.js
+++ modulo-3/modulo-3-leccion-8-ejercicio-8.3.1-rutas-estaticas/src/components/App.js
@@ -1,4 +1,5 @@
 import '../styles/App.scss';
+import { Link, Route, Routes } from 'react-router-dom';
 // import callToApi from '../services/api';
 // import { useEffect, useState } from 'react';
 // import ls from '../services/localStorage';
@@ -6,7 +7,43 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header>
+        <h1>Copiando a Github</h1>
+        <Routes>
+          <Route path="/repositories" />
+          <Route path="/packages" />
+          <Route path="/people" />
+          <Route path="/teams" />
+          <Route path="/projects" />
+          <Route path="/settings" />
+        </Routes>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="#">Overview</Link>
+            </li>
+            <li>
+              <Link to="#">Repositories</Link>
+            </li>
+            <li>
+              <Link to="#">Packages</Link>
+            </li>
+            <li>
+              <Link to="#">People</Link>
+            </li>
+            <li>
+              <Link to="#">Teams</Link>
+            </li>
+            <li>
+              <Link to="#">Projects</Link>
+            </li>
+            <li>
+              <Link to="#">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
