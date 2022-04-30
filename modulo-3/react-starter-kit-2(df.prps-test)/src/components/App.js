0c0669e26a996f4a7b27431f5a0c9d377b076de5
SandraRial <sandrarialperez@gmail.com>
2022-04-30 11:59:22 +0200

    add template

---
+++ modulo-3/react-starter-kit-2(df.prps-test)/src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.scss';
+// import callToApi from '../services/api';
+// import { useEffect, useState } from 'react';
+// import ls from '../services/localStorage';
+//import PropTypes from 'prop-types';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
