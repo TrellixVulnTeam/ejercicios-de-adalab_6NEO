076c341aab61a475b4d5844aaeb0521a784309ec
SandraRial <sandrarialperez@gmail.com>
2022-05-11 17:34:36 +0200

    server- endpoints -fail

--- modulo-4/modulo-4-leccion-2-ejercicio-2.1.2-cambio-de-endpoints-y-verbos/public/js/main.js
+++ modulo-4/modulo-4-leccion-2-ejercicio-2.1.2-cambio-de-endpoints-y-verbos/public/js/main.js
@@ -1,9 +1,9 @@
 // get users data
 
 document.querySelector('.js-btn-get-users').addEventListener('click', () => {
-  fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(responseData => {
+  fetch('http://localhost:3500/users')
+    .then((response) => response.json())
+    .then((responseData) => {
       console.log('Data:', responseData);
       printJson('.js-get-users-result', responseData);
     });
@@ -11,24 +11,26 @@ document.querySelector('.js-btn-get-users').addEventListener('click', () => {
 
 // post new user
 
-document.querySelector('.js-btn-post-new-user').addEventListener('click', () => {
-  const requestData = {
-    userName: document.querySelector('.js-username').value
-  };
+document
+  .querySelector('.js-btn-post-new-user')
+  .addEventListener('click', () => {
+    const requestData = {
+      userName: document.querySelector('.js-username').value,
+    };
 
-  fetch('http://localhost:3000/new-user', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(requestData)
-  })
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('Data:', responseData);
-      printJson('.js-get-post-new-user-result', responseData);
-    });
-});
+    fetch('http://localhost:3500/users/add', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log('Data:', responseData);
+        printJson('.js-get-post-new-user-result', responseData);
+      });
+  });
 
 // helper
 
