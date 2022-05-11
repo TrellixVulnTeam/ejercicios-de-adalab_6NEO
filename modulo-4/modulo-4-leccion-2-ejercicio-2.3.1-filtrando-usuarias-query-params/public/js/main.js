1bb63a51e6b8a4ae5857500ec84e3071719a2c6d
SandraRial <sandrarialperez@gmail.com>
2022-05-11 21:21:36 +0200

    add exercice

--- modulo-4/modulo-4-leccion-2-ejercicio-2.3.1-filtrando-usuarias-query-params/public/js/main.js
+++ modulo-4/modulo-4-leccion-2-ejercicio-2.3.1-filtrando-usuarias-query-params/public/js/main.js
@@ -2,27 +2,30 @@
 
 // post new user
 
-document.querySelector('.js-btn-post-new-user').addEventListener('click', () => {
-  const inputName = document.querySelector('.js-input-name');
-  const inputEmail = document.querySelector('.js-input-email');
+document
+  .querySelector('.js-btn-post-new-user')
+  .addEventListener('click', () => {
+    const inputName = document.querySelector('.js-input-name');
+    const inputEmail = document.querySelector('.js-input-email');
+    const inputFilter = document.querySelector('.js-input-filter');
 
-  // create query params
-  const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}`;
+    // create query params
+    const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}&filterByName=${inputFilter.value}`;
 
-  fetch('http://localhost:3000/user' + queryParams, { method: 'POST' })
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('Server response:', responseData);
-      printJson('.js-post-new-user-result', responseData);
-    });
-});
+    fetch('http://localhost:3000/user' + queryParams, { method: 'POST' })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log('Server response:', responseData);
+        printJson('.js-post-new-user-result', responseData);
+      });
+  });
 
 // get users data
 
 document.querySelector('.js-btn-get-users').addEventListener('click', () => {
   fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(responseData => {
+    .then((response) => response.json())
+    .then((responseData) => {
       console.log('Server response:', responseData);
       printJson('.js-get-users-result', responseData);
     });
